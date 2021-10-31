9e6ba5e2a023b270df04a2d99ea31615ceeb5d70
krishnaCheashty <krishnacheashty@gmail.com>
2021-10-31 21:27:24 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -37,9 +37,9 @@ function App() {
             <Route path='/login'>
               <Login></Login>
             </Route>
-            <Route path='/myOrder'>
+            <PrivateRoute path='/myOrder'>
               <MyOrder></MyOrder>
-            </Route>
+            </PrivateRoute>
             <PrivateRoute path='/manageAllOrder'>
               <ManageAllSerVice></ManageAllSerVice>
             </PrivateRoute>
