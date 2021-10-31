299ddca3909788e2577bf9f0c4ca7fbcd64cd431
krishnaCheashty <krishnacheashty@gmail.com>
2021-10-31 23:48:53 +0600

    table

--- src/pages/Home/Manageallservice/ManageAllSerVice.js
+++ src/pages/Home/Manageallservice/ManageAllSerVice.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Container, Table } from 'react-bootstrap';
+import useAuth from '../../Hooks/useAuth';
 
 const ManageAllSerVice = () => {
+    const{user}=useAuth()
     return (
-        <div>
-            manage all order
-        </div>
+        <Container>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                    <th>#</th>
+                    <th>User Name</th>
+                    <th>User Email</th>
+                    <th>Event Name</th>
+                    <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>1</td>
+                    <td>{user.displayName}</td>
+                    <td>{user.email}</td>
+                    <td></td>
+                    <td><button>Delete</button></td>
+                    </tr>
+                </tbody>
+                </Table>
+        </Container>
     );
 };
 
